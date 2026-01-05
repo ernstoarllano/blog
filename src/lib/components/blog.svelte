39db9c4222351e6c8dd521b517fc03b50fe93491
Ernesto Arellano <ernestoarellano@icloud.com>
2026-01-04 18:24:38 -0700

    new blog post

--- src/lib/components/blog.svelte
+++ src/lib/components/blog.svelte
@@ -4,11 +4,15 @@
         <div>
             <ul>
                 <li class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
+                    <a class="text-sm text-zinc-300 transition-all" href="/blog/embracing-the-ai-first-paradigm">Embracing the AI-First Paradigm</a>
+                    <div class="grow mx-2 border-b border-dashed border-zinc-400 hidden sm:block"></div>
+                    <time class="grow-0 text-xs text-zinc-400" datetime="2026-01-04">Jan 4, 26</time>
+                </li>
+                <li class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
                     <a class="text-sm text-zinc-300 transition-all" href="/blog/a-proactive-approach-to-platform-stability">A Proactive Approach to Platform Stability</a>
                     <div class="grow mx-2 border-b border-dashed border-zinc-400 hidden sm:block"></div>
                     <time class="grow-0 text-xs text-zinc-400" datetime="2025-11-26">Nov 26, 25</time>
-                </li>
-                
+                </li>                
                 <li class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
                     <a class="text-sm text-zinc-300" href="/blog/building-form-abstractions-that-multiply-team-velocity">Building Form Abstractions That Multiply Team Velocity</a>
                     <div class="grow mx-2 border-b border-dashed border-zinc-400 hidden sm:block"></div>
@@ -22,7 +26,7 @@
                 <li class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
                     <a class="text-sm text-zinc-300" href="/blog/multi-tenant-email-architecture">Multi-Tenant Email Architecture</a>
                     <div class="grow mx-2 border-b border-dashed border-zinc-400 hidden sm:block"></div>
-                    <time class="grow-0 text-xs text-zinc-400" datetime="2023-08-01">Aug 1, 25</time>
+                    <time class="grow-0 text-xs text-zinc-400" datetime="2023-08-01">Aug 1, 23</time>
                 </li>
             </ul>
         </div>
